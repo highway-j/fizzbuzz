5fae5c31c3e12691e9d5e6258f268368c14d34b1
Jonathan Kim <jckim134@gmail.com>
2015-05-15 12:22:55 -0700

    added spaces

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
 	for (var i = 1; i <= 100; i++) {
 		if (i % 3 == 0 && i % 5 == 0) {
-			document.write("Fizz Buzz");
+			document.write("Fizz Buzz\n");
 			console.log("Fizz Buzz");
 		} else if (i % 3 == 0) {
-			document.write("Fizz");
+			document.write("Fizz\n");
 			console.log("Fizz");
 		} else if (i % 5 == 0) {
-			document.write("Buzz");
+			document.write("Buzz\n");
 			console.log("Buzz");
 		} else {
-			document.write(i);
+			document.write(i + "\n");
 			console.log(i);
 		};
 	};
